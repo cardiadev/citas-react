80675fe7290e5e6c6d739715e1dfa924bf7ea10f
Carlos Javier Diaz Flores <carlosdiaz.dev@gmail.com>
2022-03-31 01:27:32 -0600

    Adding form of pets inputs and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
-
   return (
-    <>
-    <Header/>
-    <Formulario/>
-    <ListadoPacientes/>
-    </>
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario />
+        <ListadoPacientes />
+      </div>
+    </div>
   );
 }
 
